fa21660cb99b9d12d14ca7ba815d1a2f73a1a1f0
leandronunesdev <nunes.pessoal@gmail.com>
2022-04-05 19:34:10 -0300

    228. Delaying Search Input

---
+++ index.js
@@ -0,0 +1,24 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'a6111738',
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+
+input.addEventListener('input', onInput);
